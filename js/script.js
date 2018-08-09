71d30aaec8065c2d2fdb8786f2ba6ce1674e0499
jaybaybremner <j.bremner@ymail.com>
2018-08-09 14:06:32 +0100

    adding arrow icon to portfolio

--- js/script.js
+++ js/script.js
@@ -5,24 +5,57 @@
 * @name scrollToLink
 * @desc to create a scrolling effect on links
 */
-function scrollToLink() {
-      const root = $("html, body");
-      $(".mainNavLink, .linkToNav").on("click", function(e){
-        // disabling default behaviour for click events on our links
-        let target = e.target.getAttribute("href");
-        if (target !== 'blog.html') {
-        e.preventDefault();
-      // animating whole website
-            root.animate(
-              {
-              "scrollTop" : $(target).offset().top -10
-          },
-            600
-          );
-        };
-    });
+const currentUrl = $(location).attr('href');
+console.log(currentUrl);
+const blogPageUrl = 'file:///Users/James/Documents/My%20Documents/Training%20Dragon/Courses/WD_2018_04_16/Web%20Design%20Home%20Project/blog1.html';
+const jqueryEffect = function(){
+  $(".mainNavLink, .linkToNav").on("click", function(e){
 
-  };// scrollToLink
+    // disabling default behaviour for click events on our links
+    e.preventDefault();
+    let target = e.currentTarget.getAttribute("href");
+    console.log(target);
+   // animating whole website
+     $("html, body")
+      .animate(
+        {
+          "scrollTop" : $(target).offset().top -10
+        },
+        600
+      );
+     });//on click
+
+}
+  const scrollToLink = function(){
+    //if not on blog page, select links from nav and link from other nav and apply jquery effect. Else if on blog only apply the jquery effect on the back to top link
+  if(currentUrl !== blogPageUrl){
+    jqueryEffect();
+}else{
+  $(".linkToNav").on("click", function(e){
+
+    // disabling default behaviour for click events on our links
+    e.preventDefault();
+    let target = e.currentTarget.getAttribute("href");
+    console.log(target);
+   // animating whole website
+     $("html, body")
+      .animate(
+        {
+          "scrollTop" : $(target).offset().top -10
+        },
+        600
+      );
+     });//on click
+
+
+   }; //end of if else statement
+
+
+};// scrollToLink
+
+
+
+
 
 /**
 * @name hideDiv
@@ -30,7 +63,8 @@ function scrollToLink() {
 */
     const hideDiv = function(){
       const duration = 300;
-      const topOfDiv1 = $(".portfolio-container").offset().top;
+      const topOfDiv1 = $(".portfolio-container, .courses").offset().top;
+      console.log('portfolio');
       $(window).scroll(function(){
         if($(this).scrollTop() > 500){
           $('.toNav').fadeIn(200);
@@ -42,14 +76,15 @@ function scrollToLink() {
 
 
 
-
-
-
 //init function
-
-  const init = function(){
+const init = function(){
     scrollToLink();
     hideDiv();
+
+
   };//init
-init();
-}());
+
+
+window.addEventListener("load", init);
+
+}()); //end of Iife
